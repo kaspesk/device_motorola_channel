e08df773728da3b3f878c23176f301176c55252c
SebaUbuntu <seba@sebaubuntu.dev>
2024-08-19 12:46:29 -0300

    channel: Move to SPDX license identifier Change-Id: I84a0885ec1aee45b8b57b139fbbf195cab1b96bf

--- lineage_channel.mk
+++ lineage_channel.mk
@@ -1,3 +1,8 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
