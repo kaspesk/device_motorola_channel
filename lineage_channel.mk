7d53c18168f188ac09ce9a939be31d9515cd284d
L1ghtzin <lightzin021@gmail.com>
2024-09-05 18:55:25 -0300

    channel: Build GAPPS

--- lineage_channel.mk
+++ lineage_channel.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Gapps
+WITH_GMS=true
+
 BUILD_FINGERPRINT := "motorola/channel_retail/channel:10/QPYS30.85-23-8-2/c00f57:user/release-keys"
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := channel
