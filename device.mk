b29f9b36d063a2bd34f189a91fa78dd7b792b729
L1ghtzin <lightzin021@gmail.com>
2024-09-08 11:19:58 -0300

    channel: Set TARGET_EXCLUDES_AUDIOFX to true

--- device.mk
+++ device.mk
@@ -53,6 +53,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_tas2560.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tas2560.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
