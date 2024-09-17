3d20198212771d312155f78991bed84d9a2bb598
whyredfire <whyredfire@gmail.com>
2024-09-16 23:49:50 -0300

    channel: Build libutils.vendor E vndksupport: Could not load libutils.so from sphal namespace: dlopen failed: library "libutils.so" not found.
    Change-Id: I5bd9623fa7a25efd5c429bac3eca78e10855219e

--- device.mk
+++ device.mk
@@ -495,6 +495,9 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
 
+PRODUCT_PACKAGES += \
+    libutils.vendor
+
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
